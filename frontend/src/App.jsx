534f7f99f6e9a108d9466cf3755b13831d2a4063
Dvinish <dvinish6669@gmail.com>
2025-02-18 22:16:21 +0530

    made front page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1  className='title'>Share your file </h1>
+     <div className="main">
+      <div className="upload">
+          <button>
+            upload
+          </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <p className='orclass'> or</p>
+      <div className="recieve">
+        <button>recieve</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+     </div>
     </>
   )
 }
