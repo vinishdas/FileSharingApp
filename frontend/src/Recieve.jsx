9149dab49824727bc204dd2af209b488299dcd13
Tapan003 <151986758+Tapan003@users.noreply.github.com>
2025-02-20 21:14:34 +0530

    t:made the receive page, change the font or something when we discuss about this

--- frontend/src/Recieve.jsx
+++ frontend/src/Recieve.jsx
@@ -1,8 +1,35 @@
 import "./App.css";
+import "./Receive.css";
+import { useState } from 'react';
+
 
 export default function Recieve({onUpdate}){
+  const [token, setToken] = useState('');
     return(<>
      <button className="uploadBack" onClick={()=>onUpdate("Home")}>Back</button>
-       <h1> heyy</h1>
+      <div className="input">
+      <h1 className="heading">Receive Files</h1>
+      <form>
+        <div className="input-container">
+          <label htmlFor="token" className="label">
+            Enter your token
+          </label>
+          <input
+            type="text"
+            id="token"
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+            className="input2"
+            placeholder="Enter the file access token"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+        >
+         submit
+        </button>
+      </form>
+    </div>
     </>)
 }
