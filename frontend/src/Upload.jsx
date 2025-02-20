77be357e28fe9727a111d4f23b37080799788d47
Dvinish <dvinish6669@gmail.com>
2025-02-20 22:57:46 +0530

    addef feature to add mulitple files

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -1,8 +1,22 @@
 import './App.css'
+import { useState } from 'react'
 
 export default function Upload({onUpdate}){
+
+const [file,setfile]=useState([]);
+    const change=(event)=>{
+        setfile([...event.target.files])
+    }
     return(<>
         <button className="uploadBack" onClick={()=>onUpdate("Home")}>Back</button>
         <p>upload</p>
+        <input placeholder="Enter your text..." class="input" name="file" type="file" multiple onChange={change}/>
+        {file.length>0&&(
+            <ul>
+                {file.map((file,index)=>(
+                    <li key={index}>{file.name}</li>
+                ))}
+            </ul>
+        )}
     </>)
 }
