e5df8f089e4cef3f37e9e8a0462dabdf1da5999b
Dvinish <dvinish6669@gmail.com>
2025-02-21 23:54:59 +0530

    added css to upoad a lil bit

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -1,22 +1,37 @@
-import './App.css'
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
 
-export default function Upload({onUpdate}){
+export default function Upload({ onUpdate }) {
+  const [file, setfile] = useState([]);
+  const change = (event) => {
+    setfile([...event.target.files]);
+  };
+  return (
+    <>
+      <button className="uploadBack" onClick={() => onUpdate("Home")}>
+        Back
+      </button>
+      <p>upload</p>
+      <label for="images" class="drop-container" id="dropcontainer">
+        <span class="drop-title">Drop files here</span>
+        or
+        <input
+          placeholder="Enter your text..."
+          class="input"
+          name="file"
+          type="file"
+          multiple
+          onChange={change}
+        />
+      </label>
 
-const [file,setfile]=useState([]);
-    const change=(event)=>{
-        setfile([...event.target.files])
-    }
-    return(<>
-        <button className="uploadBack" onClick={()=>onUpdate("Home")}>Back</button>
-        <p>upload</p>
-        <input placeholder="Enter your text..." class="input" name="file" type="file" multiple onChange={change}/>
-        {file.length>0&&(
-            <ul>
-                {file.map((file,index)=>(
-                    <li key={index}>{file.name}</li>
-                ))}
-            </ul>
-        )}
-    </>)
+      {file.length > 0 && (
+        <ul>
+          {file.map((file, index) => (
+            <li  key={index}>{file.name}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
