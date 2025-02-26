f1efa5570d25f478c84ec259e0fab7a3bcf3121f
Dvinish <dvinish6669@gmail.com>
2025-02-26 15:45:18 +0530

    added schema

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Grid = require('gridfs-stream');
+const { MdFileCopy } = require('react-icons/md');
 require('dotenv').config();
 
 const app = express(); 
-const mongoURI = ' ';
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+const schema =new mongoose.Schema({
+    folderName: { type: String, required: true }, 
+    folderId: { type: String, required: true, unique: true },  
+    uploadDate: { type: Date, default: Date.now },
+    expireAt: { type: Date, required: true }, 
+    downloadUrl: { type: String, required: true }, 
+    token: { type: String, required: true, unique: true }, 
+    files: [{ type: mongoose.Schema.Types.ObjectId, ref: "File" }],  
+    isExpired: { type: Boolean, default: false }
+    
+})
+
+const FileColl= mongoose.model("FileColl",schema);
+module.exports = FileColl;
+
+
 app.use(cors());
 app.use(express.json());
 
