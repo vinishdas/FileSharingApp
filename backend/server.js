2b88628f1a7844216db39ac78fe17ee3d0aacb2e
Dvinish <dvinish6669@gmail.com>
2025-02-27 23:02:20 +0530

    tryed to add get

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const File = require('./model')
 const { MdFileCopy } = require('react-icons/md');
 require('dotenv').config();
 
-const app = express(); 
-mongoose.connect(process.env.MONGO_URI,{
+const app = express();
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useUnifiedTopology: true,
 })
-.then(() => console.log("Connected to MongoDB Atlas"))
-.catch(err => console.error("MongoDB connection error:", err));
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
-const schema =new mongoose.Schema({
-    folderName: { type: String, required: true }, 
-    folderId: { type: String, required: true, unique: true },  
-    uploadDate: { type: Date, default: Date.now },
-    expireAt: { type: Date, required: true }, 
-    downloadUrl: { type: String, required: true }, 
-    token: { type: String, required: true, unique: true }, 
-    files: [{ type: mongoose.Schema.Types.ObjectId, ref: "File" }],  
-    isExpired: { type: Boolean, default: false }
-    
-})
 
-const FileColl= mongoose.model("FileColl",schema);
-module.exports = FileColl;
 
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("hello")
-})
+ app.post("/upload",(req,res)=>{
+    try{
+        
+    }
+ })
 
 const PORT = 5000;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server is runnig on ${PORT}`);
 })
