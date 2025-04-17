9152881e03aeb3c740162db8ae3e853cdbae24b1
Dvinish <dvinish6669@gmail.com>
2025-04-18 02:42:49 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -166,8 +166,7 @@ async function extractWtTokenFromDownloadPage(downloadPageUrl) {
     const browser = await puppeteer.launch({
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      executablePath:
-        process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
+      executablePath: await puppeteer.executablePath(),
     });
   
     const page = await browser.newPage();
